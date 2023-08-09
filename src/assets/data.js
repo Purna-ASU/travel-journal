ab904c6c74a6ea7832bb243b86d1da5a9432af9c
Purna-ASU <ppeddire@asu.edu>
2023-08-08 17:31:36 -0700

    Initial Commit

---
+++ src/assets/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Grand Canyon",
+        location: "Arizona, United States",
+        googleMapsUrl: "https://goo.gl/maps/oFz2hq6rAukHFDoi8",
+        startDate: "20 May, 2022",
+        endDate: "28 May, 2022",
+        description: "The Grand Canyon is a breathtaking natural wonder carved by the Colorado River. It's known for its immense size and vibrant geological history, attracting millions of visitors annually.",
+        imageUrl: "https://source.unsplash.com/G-pniwaSaSw"
+    },
+    {
+        title: "Santorini",
+        location: "Greece",
+        googleMapsUrl: "https://goo.gl/maps/GktSgfNqD3yvKx9s7",
+        startDate: "8 Sep, 2022",
+        endDate: "15 Sep, 2022",
+        description: "Santorini is a stunning island in the Aegean Sea known for its white-washed buildings, crystal-clear waters, and stunning sunsets. It's a popular destination for couples and honeymooners.",
+        imageUrl: "https://source.unsplash.com/EJySz-78KNE"
+    },
+    {
+        title: "Machu Picchu",
+        location: "Cusco, Peru",
+        googleMapsUrl: "https://goo.gl/maps/MbqgDkCnZqm6fF5N6",
+        startDate: "3 Apr, 2023",
+        endDate: "10 Apr, 2023",
+        description: "Machu Picchu is an ancient Incan citadel located high in the Andes Mountains. It's renowned for its archaeological significance, stunning vistas, and the challenging Inca Trail.",
+        imageUrl: "https://source.unsplash.com/ThzFH3G-Rv8"
+    }
+];
